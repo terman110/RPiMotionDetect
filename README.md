e58cc1b3fbdb010c4f814420257ecfe23e823b6b
Jan <dev@janbeneke.de>
2016-05-05 19:02:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+RPiMotionDetec
+===========
+
+##Introduction
+
+A python script for the Raspberry PI B (and newer) dat controls a relay depending on motion. It uses a HC-SR501 PIR motion detector (pin 24, GND to GND on pi, 5v to 5V on pi) and a one port 5V relay (pin 23, GND to GND on pi, 5V to 3.3V on pi) module.
+
+[[https://github.com/terman110/repository/RPiMotionDetec/master/img/gpio.png|alt=GPIOs]]
+[[https://github.com/terman110/repository/RPiMotionDetec/master/img/pir.jog|alt=PIR]]
+
+##Auto run on pi
+
+We need to make the launcher script an executable, which we do with this command
+    chmod 775 launcher.sh
+
+Now test it
+    sh launcher.sh
+
+Add to crontab
+    sudo crontab -e
+
+Add line
+    @reboot sh /home/pi/projects/RPiMotionDetec/launcher.sh >/home/pi/projects/RPiMotionDetec/log/log.txt 2>&1
---
+++ motion.py
@@ -0,0 +1,49 @@
+import RPi.GPIO as GPIO
+import time
+
+max_timeout_s = 120
+last_motion = 0
+is_motion = False
+
+GPIO.setmode(GPIO.BCM)
+PIR_PIN = 24
+RLS_PIN = 23
+GPIO.setup(PIR_PIN, GPIO.IN)
+GPIO.setup(RLS_PIN, GPIO.OUT)
+
+def logMsg(msg):
+    print '[' . time.ctime() . '] ' + msg
+
+def OnMotion(PIR_PIN):
+        global last_motion
+        global is_motion
+        is_motion = GPIO.input(PIR_PIN)
+        if is_motion:
+                last_motion = time.time()
+                logMsg("Motion detected!")
+                if GPIO.input(RLS_PIN):
+                        GPIO.output(RLS_PIN, GPIO.LOW)
+                logMsg("Power ON")
+        else:
+                logMsg("No one there")
+
+logMsg("Motion Controlled Power Module")
+time.sleep(2)
+logMsg("Ready")
+
+try:
+        is_motion = GPIO.input(PIR_PIN)
+        last_motion = time.time()
+        if(is_motion):
+                GPIO.output(RLS_PIN, GPIO.LOW)
+        else:
+                GPIO.output(RLS_PIN, GPIO.HIGH)
+        GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=OnMotion, bouncetime=300)
+        while 1:
+                if(not is_motion and not GPIO.input(RLS_PIN) and (time.time() - last_motion) >= max_timeout_s):
+                        GPIO.output(RLS_PIN, GPIO.HIGH)
+                        logMsg("Power OFF")
+                        time.sleep(1)
+except KeyboardInterrupt:
+        logMsg("Quit")
+        GPIO.cleanup()
