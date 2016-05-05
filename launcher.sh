9363427d0e7c2517cbbaa7d67f1105236fe19bd0
Jan (Mirror) <dev@janbeneke.de>
2016-05-05 20:16:11 +0200

    logging fixes

--- launcher.sh
+++ launcher.sh
@@ -3,5 +3,5 @@
 # navigate to home directory, then to this directory, then execute python script, then back home
 cd /
 cd /home/pi/projects/RPiMotionDetect
-sudo python motion.py
+sudo python motion.py >> log/log.txt 2>&1 &
 cd /
